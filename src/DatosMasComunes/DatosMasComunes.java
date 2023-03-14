6241f5d08e2d9cbaeeebbc68604599da3e7beea6
Cristian Mercado <crismer1988@gmail.com>
2023-03-13 21:12:50 -0500

    Done

--- src/DatosMasComunes/DatosMasComunes.java
+++ src/DatosMasComunes/DatosMasComunes.java
@@ -1,2 +1,37 @@
-package DatosMasComunes;public class DatosMasComunes {
+package DatosMasComunes;
+
+public class DatosMasComunes {
+    public static void main(String[] args) {
+
+        //numeric:
+
+        int numero1 = 10;
+        byte numero2 = 1;
+        double numero3 = 10.1D;
+        float numero4 = 10.5F;
+        short numero5 = 20;
+        long numero6 = 100;
+
+        //booleans:
+
+        boolean valor1 = true;
+        boolean valor2 = false;
+
+        //text
+
+        String texto1 = "Cristian Mercado";
+        char texto2 = 'a';
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(numero6);
+        System.out.println(valor1);
+        System.out.println(valor2);
+        System.out.println(texto1);
+        System.out.println(texto2);
+
+    }
 }
